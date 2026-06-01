41b1e1bdbdb613bc02ab0cdcfaf119f606ad3ec6
RicardoSanzonetti <ricardosanzonetti.tech@gmail.com>
2026-06-01 22:14:41 +0200

    fix: add default settings for CI

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -6,11 +6,11 @@ class Settings(BaseSettings):
     APP_VERSION: str = "0.1.0"
     ENVIRONMENT: str = "development"
 
-    POSTGRES_DB: str
-    POSTGRES_USER: str
-    POSTGRES_PASSWORD: str
-    POSTGRES_HOST: str
-    POSTGRES_PORT: int
+    POSTGRES_DB: str = "trusthire"
+    POSTGRES_USER: str = "trusthire"
+    POSTGRES_PASSWORD: str = "trusthire" 
+    POSTGRES_HOST: str = "postgres"
+    POSTGRES_PORT: int = 5432
 
     model_config = SettingsConfigDict(
         env_file=".env",
