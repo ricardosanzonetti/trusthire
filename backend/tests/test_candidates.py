43a035aa5e14370e21186580545042bc96b32ee7
RicardoSanzonetti <ricardosanzonetti.tech@gmail.com>
2026-06-01 23:14:18 +0200

    test: use health endpoint in CI

--- backend/tests/test_candidates.py
+++ backend/tests/test_candidates.py
@@ -6,6 +6,6 @@ from app.main import app
 client = TestClient(app)
 
 
-def test_health_candidates_endpoint():
-    response = client.get("/candidates/")
+def test_health_endpoint():
+    response = client.get("/health")
     assert response.status_code == 200
