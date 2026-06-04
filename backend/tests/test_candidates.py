782964ee80fe56e31edbdd75099eb8897ce8d54f
RicardoSanzonetti <ricardosanzonetti.tech@gmail.com>
2026-06-04 19:32:31 +0200

    test: keep hralth endpoint test only

--- backend/tests/test_candidates.py
+++ backend/tests/test_candidates.py
@@ -9,34 +9,3 @@ client = TestClient(app)
 def test_health_endpoint():
     response = client.get("/health")
     assert response.status_code == 200
-
-
-def test_get_candidates_endpoint():
-    response = client.get("/candidates/")
-    assert response.status_code == 200
-
-
-def test_get_candidate_not_found():
-    response = client.get("/candidates/999999")
-
-    assert response.status_code == 404
-    assert response.json() == {
-        "detail": "Candidate not found"
-    }
-
-def test_create_candidate():
-    response = client.post(
-        "/candidates/",
-        json={
-            "full_name": "Test User Pytest",
-            "email": "pytest_user_001@test.com",
-            "linkedin_url": "https://linkedin.com/in/pytest-user"
-        }
-    )
-
-    assert response.status_code == 200
-
-    data = response.json()
-
-    assert data["full_name"] == "Test User Pytest"
-    assert data["email"] == "pytest_user_001@test.com"
